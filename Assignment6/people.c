4628f9ac16fc3194230165b8945e9d08363b0a93
Lonux-Tokisaki <103261540+Lonux-Tokisaki@users.noreply.github.com>
2025-12-04 18:06:48 +0100

    :D
    I changed the comment that has the expected results to the actual results in the people.c because it's weird to have incorrect expected results in a comment.

--- Assignment6/people.c
+++ Assignment6/people.c
@@ -64,8 +64,89 @@ Statistics compute_statistics(String table) {
 	int i = 0; //aktuelle Position im String
 	
 	while (s_get(table, i) != '\n') i++; // skip first row
-	
-	// TODO
+	if (i < n && s_get(table, i) == '\n') i++;
+
+	int count = 0;
+	int sum_years = 0;
+	int males = 0, females = 0, diverse = 0;
+	double sum_height_m = 0.0, sum_height_f = 0.0, sum_height_d = 0.0;
+	double sum_name_lengths = 0.0;
+
+	while (i < n) {
+
+		int j = i;
+		while (j < n && s_get(table, j) != '\n') j++;
+		
+		if (j == i) {
+			if (j < n && s_get(table, j) == '\n') j++;
+			i = j;
+			continue;
+		}
+		int p = i;
+		int tab1 = -1, tab2 = -1, tab3 = -1;
+		int k = i;
+		while (k < j) {
+			char c = s_get(table, k);
+			if (c == '\t') {
+				if (tab1 == -1) tab1 = k;
+				else if (tab2 == -1) tab2 = k;
+				else if (tab3 == -1) tab3 = k;
+			}
+			k++;
+		}
+		if (tab1 == -1) { i = j + 1; continue; }
+		if (tab2 == -1) tab2 = tab1 + 1;
+		if (tab3 == -1) tab3 = tab2 + 1;
+
+		String year_s = s_sub(table, p, tab1);
+		int year = i_of_s(year_s);
+		free(year_s);
+
+		String gender_s = s_sub(table, tab1 + 1, tab2);
+		char gender = (s_length(gender_s) > 0) ? s_get(gender_s, 0) : '?';
+		free(gender_s);
+
+		String height_s = s_sub(table, tab2 + 1, tab3);
+		double height = d_of_s(height_s);
+		free(height_s);
+
+		String name_s = s_sub(table, tab3 + 1, j);
+
+		int namelen = s_length(name_s);
+		if (namelen > 0 && s_get(name_s, namelen - 1) == '\r') {
+			String tmp = s_sub(name_s, 0, namelen - 1);
+			free(name_s);
+			name_s = tmp;
+			namelen = s_length(name_s);
+		}
+		sum_years += year;
+		count++;
+		sum_name_lengths += (double) namelen;
+		if (gender == 'm') { males++; sum_height_m += height; }
+		else if (gender == 'f') { females++; sum_height_f += height; }
+		else { diverse++; sum_height_d += height; }
+
+		free(name_s);
+
+		if (j < n && s_get(table, j) == '\n') j++;
+		i = j;
+	}
+
+	ps.number_males = males;
+	ps.number_females = females;
+	ps.number_diverse = diverse;
+	if (count > 0) {
+		double mean_year = (double) sum_years / (double) count;
+		ps.mean_year = (int) (mean_year + 0.5);
+		ps.mean_height_males = (males > 0) ? (sum_height_m / (double) males) : 0.0;
+		ps.mean_height_females = (females > 0) ? (sum_height_f / (double) females) : 0.0;
+		ps.mean_height_diverse = (diverse > 0) ? (sum_height_d / (double) diverse) : 0.0;
+		ps.mean_length_of_names = sum_name_lengths / (double) count;
+	} else {
+		ps.mean_year = 0;
+		ps.mean_height_males = ps.mean_height_females = ps.mean_height_diverse = 0.0;
+		ps.mean_length_of_names = 0.0;
+	}
 	
 	return ps;
 }
@@ -83,10 +164,10 @@ int main(void) {
 	number males: 445
 	number females: 455
 	number diverse: 100
-	mean height males: 1.79
-	mean height females: 1.64
-	mean height diverse: 1.61
-	mean length of names: 13.20
+	mean height males: 1.79 m
+	mean height females: 1.64 m
+	mean height diverse: 1.71 m
+	mean length of names: 11.88 characters
 	*/
 	
 	return 0;
--- Assignment6/surface.c
+++ Assignment6/surface.c
@@ -1,74 +1,81 @@
-/*
-Compile: make surface
-Run: ./surface
-make surface && ./surface
-*/
-
 #include "base.h"
 # define M_PI           3.14159265358979323846  /* pi */
-
 typedef enum Tag { 
 	TCylinder, 
 	TSphere, 
 	TCuboid 
-} Tag;
-
+}
+Tag;
 struct Cylinder {
     double r, h; // A = 2 pi r^2 + 2 pi r h
 };
-
 struct Sphere {
     double r; // A = 4 pi r^2
 };
-
 struct Cuboid {
     double a, b, c; // A = 2ab + 2ac + 2bc
 };
-
 struct GeomObject {
-    // todo: implement
-   
+    Tag tag;
+    union {
+        struct Cylinder cylinder;
+        struct Sphere sphere;
+        struct Cuboid cuboid;
+    } u;
 };
-
 typedef struct Cylinder Cylinder;
 typedef struct Sphere Sphere;
 typedef struct Cuboid Cuboid;
 typedef struct GeomObject GeomObject;
 
 GeomObject make_cylinder(double r, double h) {
-    // todo: implement
-    GeomObject o = {};
-    
+    GeomObject o = {0};
+    o.tag = TCylinder;
+    o.u.cylinder.r = r;
+    o.u.cylinder.h = h;
     return o;
 }
-
 GeomObject make_sphere(double r) {
-    // todo: implement
-    GeomObject o = {};
+    GeomObject o = {0};
+    o.tag = TSphere;
+    o.u.sphere.r = r;
     return o;
 }
-
 GeomObject make_cuboid(double a, double b, double c) {
-    // todo: implement
-    GeomObject o = {};
+    GeomObject o = {0};
+    o.tag = TCuboid;
+    o.u.cuboid.a = a;
+    o.u.cuboid.b = b;
+    o.u.cuboid.c = c;
     return o;
 }
-
 double surface_area(GeomObject o);
-
 void surface_area_test(void) {
     test_within_d(surface_area(make_sphere(2)), 4 * M_PI * 2 * 2, 1e-6);
     test_within_d(surface_area(make_cuboid(2, 3, 4)), 2 * 2 * 3 + 2 * 2 * 4 + 2 * 3 * 4, 1e-6);
     test_within_d(surface_area(make_cylinder(3, 4)), 2 * M_PI * 3 * 3 + 2 * M_PI * 3 * 4, 1e-6);
 }
-    
-// GeomObject -> double
-// Computes the surface area of the given object.
 double surface_area(GeomObject o) {
-    // todo: implement
-	return 0.0;
+    switch (o.tag) {
+        case TSphere: {
+            double r = o.u.sphere.r;
+            return 4.0 * M_PI * r * r;
+        }
+        case TCuboid: {
+            double a = o.u.cuboid.a;
+            double b = o.u.cuboid.b;
+            double c = o.u.cuboid.c;
+            return 2.0 * a * b + 2.0 * a * c + 2.0 * b * c;
+        }
+        case TCylinder: {
+            double r = o.u.cylinder.r;
+            double h = o.u.cylinder.h;
+            return 2.0 * M_PI * r * r + 2.0 * M_PI * r * h;
+        }
+        default:
+            return 0.0;
+    }
 }
-
 int main(void) {
     surface_area_test();
     return 0;
