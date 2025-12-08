bdeaa0cb4d0f524d6fe02751c0efbf7659b5a345
Minh Dang Hoang <hminhdang05@gmail.com>
2025-12-08 22:55:25 +0100

    feat(7.4): complete exercise 4

--- Assignment7/substitution.c
+++ Assignment7/substitution.c
@@ -1,33 +1,59 @@
 #include "base.h"
 
 const String permutation = "SGEMFVYZIDPBUKHARNXQWOLJTC";
-
+// real stuffs             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// encrypt the input
 String encrypt(String input) {
-    // TODO
-    return s_copy(input);
+  // TODO
+  String output = s_copy(input);
+  int length = s_length(input);
+  for (int index = 0; index < length; index++) {
+    char current_char = input[index];
+    if (isalpha(current_char)) {
+      int position = current_char - 'A';
+      output[index] = permutation[position];
+    }
+  }
+  return output;
 }
 
 int index_of(String s, char c) {
-    // TODO
-    return -1;
+  // TODO
+  int len = s_length(s);
+  for (int i = 0; i < len; i++) {
+    if (s[i] == c)
+      return i;
+  }
+  return -1;
 }
 
 String decrypt(String input) {
-    // TODO
-    return s_copy(input);
+  // TODO
+  String output = s_copy(input);
+  int length = s_length(input);
+  for (int index = 0; index < length; index++) {
+    char current_char = input[index];
+    if (isalpha(current_char)) {
+      int position = index_of(permutation, current_char);
+      if (position >= 0)
+        output[index] = 'A' + position;
+    }
+  }
+  return output;
 }
 
 void test_identity(String s) {
-    assert("encrypted text must differ from input", !s_equals(s, encrypt(s)));
-    test_equal_s(s, decrypt(encrypt(s)));
+  assert("encrypted text must differ from input", !s_equals(s, encrypt(s)));
+  test_equal_s(s, decrypt(encrypt(s)));
 }
 
 int main(void) {
-    test_identity("HELLO WORLD");
-    test_identity("SOME SUPER SECRET TEXT");
-    test_identity("DOES QUOTATION CAUSE ISSUES? IT SHOULDN'T.");
-    test_identity("BE SURE TO DRINK YOUR OVALTINE");
+  test_identity("HELLO WORLD");
+  test_identity("SOME SUPER SECRET TEXT");
+  test_identity("DOES QUOTATION CAUSE ISSUES? IT SHOULDN'T.");
+  test_identity("BE SURE TO DRINK YOUR OVALTINE");
 
-    printsln("The secret text says:");
-    printsln(decrypt("VNSKC DSYQ IU PHUABFQQ OFNLSZNBHXQFK QSJI RWFN MWNEZ GSTFNK."));
-}
+  printsln("The secret text says:");
+  printsln(
+      decrypt("VNSKC DSYQ IU PHUABFQQ OFNLSZNBHXQFK QSJI RWFN MWNEZ GSTFNK."));
+} // ich verstehe die Referrenz nicht (?)
