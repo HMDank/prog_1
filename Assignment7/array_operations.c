7bbd697227b0716d3c094b8acb6b973e152a2e70
Minh Dang Hoang <hminhdang05@gmail.com>
2025-12-11 20:57:46 +0100

    chore: reformat

--- Assignment7/array_operations.c
+++ Assignment7/array_operations.c
@@ -7,276 +7,273 @@ Compile & run: make array_operations && ./array_operations
 #include "base.h"
 
 typedef struct better_array_s {
-    int length;
-    int *array;
+  int length;
+  int *array;
 } BetterArray;
 
 bool compare(int *array_a, int length_a, int *array_b, int length_b) {
-    if (length_a < 0 || length_b < 0) {
-        return false;
-    }
-    
-    if (length_a != length_b) {
-        return false;
-    }
-    
-    if (length_a == 0) {
-        return true;
-    }
-    
-    for (int i = 0; i < length_a; i++) {
-        if (array_a[i] != array_b[i]) {
-            return false;
-        }
-    }
-    
+  if (length_a < 0 || length_b < 0) {
+    return false;
+  }
+
+  if (length_a != length_b) {
+    return false;
+  }
+
+  if (length_a == 0) {
     return true;
+  }
+
+  for (int i = 0; i < length_a; i++) {
+    if (array_a[i] != array_b[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-int remove_negatives(int* array, int length) {
-    int write_index = 0;
-    
-    for (int read_index = 0; read_index < length; read_index++) {
-        if (array[read_index] >= 0) {
-            array[write_index] = array[read_index];
-            write_index++;
+int remove_negatives(int *array, int length) {
+  int write_index = 0;
+
+  for (int read_index = 0; read_index < length; read_index++) {
+    if (array[read_index] >= 0) {
+      array[write_index] = array[read_index];
+      write_index++;
+    }
+  }
+
+  return write_index;
+}
+
+BetterArray intersect(int *array_a, int length_a, int *array_b, int length_b) {
+  int max_size = (length_a < length_b) ? length_a : length_b;
+
+  int *result = xcalloc(max_size, sizeof(int));
+  int result_length = 0;
+
+  for (int i = 0; i < length_a; i++) {
+    bool found_in_b = false;
+    for (int j = 0; j < length_b; j++) {
+      if (array_a[i] == array_b[j]) {
+        found_in_b = true;
+        break;
+      }
+    }
+
+    if (found_in_b) {
+      bool already_in_result = false;
+      for (int k = 0; k < result_length; k++) {
+        if (result[k] == array_a[i]) {
+          already_in_result = true;
+          break;
         }
+      }
+
+      if (!already_in_result) {
+        result[result_length] = array_a[i];
+        result_length++;
+      }
     }
-    
-    return write_index;
+  }
+
+  return (BetterArray){.length = result_length, .array = result};
 }
 
-BetterArray intersect(int *array_a, int length_a, int* array_b, int length_b) {
-    int max_size = (length_a < length_b) ? length_a : length_b;
-    
-    int *result = xcalloc(max_size, sizeof(int));
-    int result_length = 0;
-    
-    for (int i = 0; i < length_a; i++) {
-        bool found_in_b = false;
-        for (int j = 0; j < length_b; j++) {
-            if (array_a[i] == array_b[j]) {
-                found_in_b = true;
-                break;
-            }
-        }
-        
-        if (found_in_b) {
-            bool already_in_result = false;
-            for (int k = 0; k < result_length; k++) {
-                if (result[k] == array_a[i]) {
-                    already_in_result = true;
-                    break;
-                }
-            }
-            
-            if (!already_in_result) {
-                result[result_length] = array_a[i];
-                result_length++;
-            }
-        }
+void merge_sorted_arrays(int *array_a, int length_a, int *array_b, int length_b,
+                         int *result, int length_result) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  while (i < length_a && j < length_b) {
+    if (array_a[i] <= array_b[j]) {
+      result[k] = array_a[i];
+      i++;
+    } else {
+      result[k] = array_b[j];
+      j++;
     }
-    
-    return (BetterArray) {
-        .length = result_length,
-        .array = result
-    };
+    k++;
+  }
+
+  while (i < length_a) {
+    result[k] = array_a[i];
+    i++;
+    k++;
+  }
+
+  while (j < length_b) {
+    result[k] = array_b[j];
+    j++;
+    k++;
+  }
 }
 
-void merge_sorted_arrays(int *array_a, int length_a, int* array_b, int length_b, int* result, int length_result) {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    
-    while (i < length_a && j < length_b) {
-        if (array_a[i] <= array_b[j]) {
-            result[k] = array_a[i];
-            i++;
-        } else {
-            result[k] = array_b[j];
-            j++;
-        }
-        k++;
-    }
-    
-    while (i < length_a) {
-        result[k] = array_a[i];
-        i++;
-        k++;
-    }
-    
-    while (j < length_b) {
-        result[k] = array_b[j];
-        j++;
-        k++;
-    }
+void test_compare() {
+  int a1[] = {1, 2, 3, -4, 8, 9};
+  int length_a1 = 6;
+  int a2[] = {1, 2, 3, -4, 8, 9};
+  int length_a2 = 6;
+  int a3[] = {1, -2, 3, 4, 8, 9};
+  int length_a3 = 6;
+
+  int a4[] = {1};
+  int length_a4 = 1;
+
+  int a5[] = {2};
+  int length_a5 = 1;
+
+  test_equal_b(compare(a1, length_a1, a2, length_a2), true);
+  test_equal_b(compare(a1, length_a1, a2, 1), false);
+  test_equal_b(compare(a1, 0, a2, length_a2), false);
+  test_equal_b(compare(NULL, 0, NULL, 0), true);
+  test_equal_b(compare(a3, 1, a2, 1), true);
+  test_equal_b(compare(a3, length_a3, a2, length_a2), false);
+  test_equal_b(compare(a3, 2, a2, 2), false);
+  test_equal_b(compare(a1, -2, a2, 2), false);
+  test_equal_b(compare(a1, -2, a2, -2), false);
+  test_equal_b(compare(a1, -2, a2, -1), false);
+  test_equal_b(compare(a1, 2, a2, -2), false);
+  test_equal_b(compare(a4, length_a4, a4, length_a4), true);
+  test_equal_b(compare(a1, 1, a4, length_a4), true);
+  test_equal_b(compare(a1, length_a1, a4, length_a4), false);
+  test_equal_b(compare(a4, length_a4, a5, length_a5), false);
 }
 
-void test_compare(){
-    int a1[] = {1, 2, 3, -4, 8, 9};
-    int length_a1 = 6;
-    int a2[] = {1, 2, 3, -4, 8, 9};
-    int length_a2 = 6;
-    int a3[] = {1, -2, 3, 4, 8, 9};
-    int length_a3 = 6;
+void test_remove_negatives() {
+  int a1[] = {1, 2, 3, -4, 8, 9};
+  int length_a1 = 6;
+  int a1_expected[] = {1, 2, 3, 8, 9};
 
-    int a4[] = {1};
-    int length_a4 = 1;
+  int a2[] = {-1, -2, -3, -4, -8, 9};
+  int length_a2 = 6;
+  int a2_expected[] = {9};
 
-    int a5[] = {2};
-    int length_a5 = 1;
+  int a3[] = {-1, -2, -3, -4, -8, -9};
+  int length_a3 = 6;
+  int a3_expected[1] = {0};
 
-    test_equal_b(compare(a1, length_a1, a2, length_a2), true);
-    test_equal_b(compare(a1, length_a1, a2, 1), false);
-    test_equal_b(compare(a1, 0, a2, length_a2), false);
-    test_equal_b(compare(NULL, 0, NULL, 0), true);
-    test_equal_b(compare(a3, 1, a2, 1), true);
-    test_equal_b(compare(a3, length_a3, a2, length_a2), false);
-    test_equal_b(compare(a3, 2, a2, 2), false);
-    test_equal_b(compare(a1, -2, a2, 2), false);
-    test_equal_b(compare(a1, -2, a2, -2), false);
-    test_equal_b(compare(a1, -2, a2, -1), false);
-    test_equal_b(compare(a1, 2, a2, -2), false);
-    test_equal_b(compare(a4, length_a4, a4, length_a4), true);
-    test_equal_b(compare(a1, 1, a4, length_a4), true);
-    test_equal_b(compare(a1, length_a1, a4, length_a4), false);
-    test_equal_b(compare(a4, length_a4, a5, length_a5), false);
+  int new_length = remove_negatives(a1, length_a1);
+  test_equal_i(new_length, 5);
+  test_equal_b(compare(a1, new_length, a1_expected, 5), true);
+
+  new_length = remove_negatives(a2, length_a2);
+  test_equal_i(new_length, 1);
+  test_equal_b(compare(a2, new_length, a2_expected, 1), true);
+
+  new_length = remove_negatives(a3, length_a3);
+  test_equal_i(new_length, 0);
+  test_equal_b(compare(a3, new_length, a3_expected, 0), true);
 }
 
-void test_remove_negatives(){
-    int a1[] = {1, 2, 3, -4, 8, 9};
-    int length_a1 = 6;
-    int a1_expected[] = {1, 2, 3, 8, 9};
+void test_intersect() {
+  int a1[] = {1, 2, 3, -4, 8, 9};
+  int length_a1 = 6;
 
-    int a2[] = {-1, -2, -3, -4, -8, 9};
-    int length_a2 = 6;
-    int a2_expected[] = {9};
+  int a2[] = {2, -2, 6, -4, -8, 10};
+  int length_a2 = 6;
 
-    int a3[] = {-1, -2, -3, -4, -8, -9};
-    int length_a3 = 6;
-    int a3_expected[1] = {0};
+  int a2_a1_intersected[] = {2, -4};
 
-    int new_length = remove_negatives(a1, length_a1);
-    test_equal_i(new_length, 5);
-    test_equal_b(compare(a1, new_length, a1_expected, 5), true);
+  int a3[] = {1, 17, 3, 5, 9, 8};
+  int length_a3 = 6;
 
-    new_length = remove_negatives(a2, length_a2);
-    test_equal_i(new_length, 1);
-    test_equal_b(compare(a2, new_length, a2_expected, 1), true);
+  int a4[] = {1, 17, 3, 5, 9};
+  int length_a4 = 5;
 
-    new_length = remove_negatives(a3, length_a3);
-    test_equal_i(new_length, 0);
-    test_equal_b(compare(a3, new_length, a3_expected, 0), true);
+  int a3_a4_intersected[] = {1, 17, 3, 5, 9};
+
+  int a5[] = {1, 3, -18};
+  int length_a5 = 3;
+
+  int a6[] = {2, -3, -19};
+  int length_a6 = 3;
+
+  int a5_a6_intersected[1] = {0};
+
+  BetterArray result = intersect(a1, length_a1, a2, length_a2);
+
+  test_equal_i(result.length, 2);
+  test_equal_b(compare(result.array, result.length, a2_a1_intersected, 2),
+               true);
+
+  result = intersect(a3, length_a3, a4, length_a4);
+
+  test_equal_i(result.length, 5);
+  test_equal_b(compare(result.array, result.length, a3_a4_intersected, 5),
+               true);
+
+  result = intersect(a5, length_a5, a6, length_a6);
+  test_equal_i(result.length, 0);
+  test_equal_b(compare(result.array, result.length, a5_a6_intersected, 0),
+               true);
 }
 
-void test_intersect(){
-    int a1[] = {1, 2, 3, -4, 8, 9};
-    int length_a1 = 6;
-    
-    int a2[] = {2, -2, 6, -4, -8, 10};
-    int length_a2 = 6;
-    
-    int a2_a1_intersected[] = {2, -4};
-    
-    
-    int a3[] = {1, 17, 3, 5, 9, 8};
-    int length_a3 = 6;
-    
-    int a4[] = {1, 17, 3, 5, 9};
-    int length_a4 = 5;
-    
-    int a3_a4_intersected[] = {1, 17, 3, 5, 9};
-    
-    
-    int a5[] = {1, 3 , -18};
-    int length_a5 = 3;
-    
-    int a6[] = {2, -3, -19};
-      int length_a6 = 3;
-    
-    int a5_a6_intersected[1] = {0};
-    
-    BetterArray result = intersect(a1, length_a1, a2, length_a2);
-    
-    test_equal_i(result.length, 2);
-    test_equal_b(compare(result.array, result.length, a2_a1_intersected, 2), true);
-    
-    result = intersect(a3, length_a3, a4, length_a4);
-    
-    test_equal_i(result.length, 5);
-    test_equal_b(compare(result.array, result.length, a3_a4_intersected, 5), true);
-    
-    result = intersect(a5, length_a5, a6, length_a6);
-    test_equal_i(result.length, 0);
-    test_equal_b(compare(result.array, result.length, a5_a6_intersected, 0), true);
-}
+void test_merge_sorted() {
+  int a1[] = {1, 2, 3, 4, 8, 9};
+  int length_a1 = 6;
 
-void test_merge_sorted(){
-    int a1[] = {1, 2, 3, 4, 8, 9};
-    int length_a1 = 6;
-    
-    int a2[] = {-2, -2, 6, 14, 15, 100};
-    int length_a2 = 6;
-    
-    int a1_a2_merged[] = {-2, -2, 1, 2, 3, 4, 6, 8, 9, 14, 15, 100};
-    
-    int result[12];
-    
-    merge_sorted_arrays(a1, length_a1, a2, length_a2, result, 12);
-    test_equal_b(compare(result, 12, a1_a2_merged, 12), true);
-    
+  int a2[] = {-2, -2, 6, 14, 15, 100};
+  int length_a2 = 6;
 
-    int a3[] = {1};
-    int length_a3 = 1;
-    
-    int a4[] = {15, 100};
-    int length_a4 = 2;
-    
-    int a3_a4_merged[] = {1, 15, 100};
-    
-    int result2[3];
-    
-    merge_sorted_arrays(a3, length_a3, a4, length_a4, result2, 3);
-    test_equal_b(compare(result2, 3, a3_a4_merged, 3), true);
-    
-    
-    int a5[] = {1, 3, 17};
-    int length_a5 = 3;
-    
-    int a6[1] = {0};
-    int length_a6 = 0;
-    
-    int a5_a6_merged[] = {1, 3, 17};
-    
-    int result3[3];
-    
-    merge_sorted_arrays(a5, length_a5, a6, length_a6, result3, 3);
-    test_equal_b(compare(result3, 3, a5_a6_merged, 3), true);
-    
-    int result4[3];
-    
-    merge_sorted_arrays(a6, length_a6, a5, length_a5, result4, 3);
-    test_equal_b(compare(result4, 3, a5_a6_merged, 3), true);
-    
-    
+  int a1_a2_merged[] = {-2, -2, 1, 2, 3, 4, 6, 8, 9, 14, 15, 100};
+
+  int result[12];
+
+  merge_sorted_arrays(a1, length_a1, a2, length_a2, result, 12);
+  test_equal_b(compare(result, 12, a1_a2_merged, 12), true);
+
+  int a3[] = {1};
+  int length_a3 = 1;
+
+  int a4[] = {15, 100};
+  int length_a4 = 2;
+
+  int a3_a4_merged[] = {1, 15, 100};
+
+  int result2[3];
+
+  merge_sorted_arrays(a3, length_a3, a4, length_a4, result2, 3);
+  test_equal_b(compare(result2, 3, a3_a4_merged, 3), true);
+
+  int a5[] = {1, 3, 17};
+  int length_a5 = 3;
+
+  int a6[1] = {0};
+  int length_a6 = 0;
+
+  int a5_a6_merged[] = {1, 3, 17};
+
+  int result3[3];
+
+  merge_sorted_arrays(a5, length_a5, a6, length_a6, result3, 3);
+  test_equal_b(compare(result3, 3, a5_a6_merged, 3), true);
+
+  int result4[3];
+
+  merge_sorted_arrays(a6, length_a6, a5, length_a5, result4, 3);
+  test_equal_b(compare(result4, 3, a5_a6_merged, 3), true);
 }
 
 int main(void) {
-    test_compare();
-    test_remove_negatives();
-    test_intersect();
-    test_merge_sorted();
+  test_compare();
+  test_remove_negatives();
+  test_intersect();
+  test_merge_sorted();
 
-    return 0;
+  return 0;
 }
-//e)
-/*Die Vorteile von BetterArray sind, dass sie die Länge mit dem Array statt das seperat einzugeben
+// e)
+/*Die Vorteile von BetterArray sind, dass sie die Länge mit dem Array statt das
+seperat einzugeben
 
 Strings werden in char-Arrays angegeben, und mit \0 beendet
 
-Die Länge eines Strings wird nicht gespeichert sondern mit dem \0 markiert. Man muss also die Zeichen bis \0 zählen
-wenn man die Länge möchte.
+Die Länge eines Strings wird nicht gespeichert sondern mit dem \0 markiert. Man
+muss also die Zeichen bis \0 zählen wenn man die Länge möchte.
 
-Sowas ist nicht für int[] möglich weil jeder int Wert als Datenwert funktioniert und dementsprechend kein Ersatz für \0
-existiert. Deswegen muss man die Länge seperat speichern.
+Sowas ist nicht für int[] möglich weil jeder int Wert als Datenwert funktioniert
+und dementsprechend kein Ersatz für \0 existiert. Deswegen muss man die Länge
+seperat speichern.
 */
--- Assignment7/rectangles.c
+++ Assignment7/rectangles.c
@@ -1,15 +1,15 @@
 #include "base.h"
 
 typedef struct draw_options_s {
-    bool has_border;
-    String fill_pattern;
+  bool has_border;
+  String fill_pattern;
 } DrawOptions;
 
 typedef struct rectangle_s {
-    int x;
-    int y;
-    int w;
-    int h;
+  int x;
+  int y;
+  int w;
+  int h;
 
 } Rectangle;
 
@@ -17,10 +17,10 @@ typedef struct rectangle_s {
  * Rauten und ohne Rahmen skizziert werden.
  */
 DrawOptions make_draw_options_flat() {
-    return (DrawOptions){
-        .has_border = false,
-        .fill_pattern = "#",
-    };
+  return (DrawOptions){
+      .has_border = false,
+      .fill_pattern = "#",
+  };
 }
 
 /* Gibt ein DrawOptions Struct zurück, das dafür sorgt dass Rechtecke mit einem
@@ -28,148 +28,142 @@ DrawOptions make_draw_options_flat() {
  * "fill" bestimmt, der ständig wiederholend in das Rechteck geschrieben wird.
  */
 DrawOptions make_draw_options_fill(String fill) {
-    require("fill is a non-empty string", s_length(fill) > 0);
+  require("fill is a non-empty string", s_length(fill) > 0);
 
-    return (DrawOptions){
-        .has_border = true,
-        .fill_pattern = fill,
-    };
+  return (DrawOptions){
+      .has_border = true,
+      .fill_pattern = fill,
+  };
 }
 
 DrawOptions make_draw_options_range(char from, char to) {
-    // todo
+  // todo
 }
 
 Rectangle make_rectangle(int x, int y, int w, int h) {
-    require("Die x Koordinate muss positiv sein", x >= 0);
-    require("Die y Koordiante muss positiv sein", y >= 0);
-    require("Die Breite muss positiv sein", w >= 0);
-    require("Die Hoehe muss positiv sein.", h >= 0);
+  require("Die x Koordinate muss positiv sein", x >= 0);
+  require("Die y Koordiante muss positiv sein", y >= 0);
+  require("Die Breite muss positiv sein", w >= 0);
+  require("Die Hoehe muss positiv sein.", h >= 0);
 
-    return (Rectangle){
-        .x = x,
-        .y = y,
-        .w = w,
-        .h = h,
-    };
+  return (Rectangle){
+      .x = x,
+      .y = y,
+      .w = w,
+      .h = h,
+  };
 }
 
 bool intersects(Rectangle a, Rectangle b) {
-    int a_right = a.x + a.w;
-    int a_bottom = a.y + a.h;
-    int b_right = b.x + b.w;
-    int b_bottom = b.y + b.h;
+  int a_right = a.x + a.w;
+  int a_bottom = a.y + a.h;
+  int b_right = b.x + b.w;
+  int b_bottom = b.y + b.h;
 
-    return !(a_right <= b.x || a.x >= b_right || a_bottom <= b.y || a.y >= b_bottom);
+  return !(a_right <= b.x || a.x >= b_right || a_bottom <= b.y ||
+           a.y >= b_bottom);
 }
 
 void print_rectangle(Rectangle rect, DrawOptions options) {
 
-    for (int i = 0; i < rect.y; i++) {
-        println("");
+  for (int i = 0; i < rect.y; i++) {
+    println("");
+  }
+
+  if (options.has_border) {
+
+    for (int i = 0; i < rect.x; i++) {
+      prints(" ");
     }
-    
-    if (options.has_border) {
-
-        for (int i = 0; i < rect.x; i++) {
-            prints(" ");
-        }
-        prints("+");
-        for (int i = 0; i < rect.w - 2; i++) {
-            prints("-");
-        }
-        printsln("+");
-
-        int fill_length = s_length(options.fill_pattern);
-        int fill_index = 0;
-        
-        for (int row = 0; row < rect.h - 2; row++) {
-
-            for (int i = 0; i < rect.x; i++) {
-                prints(" ");
-            }
-            prints("|");
-            
-
-            for (int col = 0; col < rect.w - 2; col++) {
-                char c = s_get(options.fill_pattern, fill_index % fill_length);
-                printc(c);
-                fill_index++;
-            }
-            
-            printsln("|");
-        }
-        
-        for (int i = 0; i < rect.x; i++) {
-            prints(" ");
-        }
-        prints("+");
-        for (int i = 0; i < rect.w - 2; i++) {
-            prints("-");
-        }
-        printsln("+");
-        
-    } else {
-
-        for (int row = 0; row < rect.h; row++) {
-
-            for (int i = 0; i < rect.x; i++) {
-                prints(" ");
-            }
-            
-            for (int col = 0; col < rect.w; col++) {
-                prints(options.fill_pattern);
-            }
-            
-            println("");
-        }
+    prints("+");
+    for (int i = 0; i < rect.w - 2; i++) {
+      prints("-");
     }
+    printsln("+");
+
+    int fill_length = s_length(options.fill_pattern);
+    int fill_index = 0;
+
+    for (int row = 0; row < rect.h - 2; row++) {
+
+      for (int i = 0; i < rect.x; i++) {
+        prints(" ");
+      }
+      prints("|");
+
+      for (int col = 0; col < rect.w - 2; col++) {
+        char c = s_get(options.fill_pattern, fill_index % fill_length);
+        printc(c);
+        fill_index++;
+      }
+
+      printsln("|");
+    }
+
+    for (int i = 0; i < rect.x; i++) {
+      prints(" ");
+    }
+    prints("+");
+    for (int i = 0; i < rect.w - 2; i++) {
+      prints("-");
+    }
+    printsln("+");
+
+  } else {
+
+    for (int row = 0; row < rect.h; row++) {
+
+      for (int i = 0; i < rect.x; i++) {
+        prints(" ");
+      }
+
+      for (int col = 0; col < rect.w; col++) {
+        prints(options.fill_pattern);
+      }
+
+      println("");
+    }
+  }
 }
 
 void test_intersects(void) {
-    test_equal_i(intersects(
-        make_rectangle(0, 0, 10, 10),
-        make_rectangle(32, 32, 10, 10)
-    ), false);
+  test_equal_i(
+      intersects(make_rectangle(0, 0, 10, 10), make_rectangle(32, 32, 10, 10)),
+      false);
 
-    test_equal_i(intersects(
-        make_rectangle(100, 100, 32, 16),
-        make_rectangle(50, 32, 50, 10)
-    ), false);
+  test_equal_i(intersects(make_rectangle(100, 100, 32, 16),
+                          make_rectangle(50, 32, 50, 10)),
+               false);
 
-    test_equal_i(intersects(
-        make_rectangle(50, 32, 50, 10),
-        make_rectangle(100, 100, 32, 16)
-    ), false);
+  test_equal_i(intersects(make_rectangle(50, 32, 50, 10),
+                          make_rectangle(100, 100, 32, 16)),
+               false);
 
-    test_equal_i(intersects(
-        make_rectangle(16, 16, 16, 16),
-        make_rectangle(16, 16, 4, 4)
-    ), true);
+  test_equal_i(
+      intersects(make_rectangle(16, 16, 16, 16), make_rectangle(16, 16, 4, 4)),
+      true);
 
-    test_equal_i(intersects(
-        make_rectangle(16, 16, 16, 16),
-        make_rectangle(8, 20, 32, 4)
-    ), true);
+  test_equal_i(
+      intersects(make_rectangle(16, 16, 16, 16), make_rectangle(8, 20, 32, 4)),
+      true);
 
-    test_equal_i(intersects(
-        make_rectangle(16, 16, 16, 16),
-        make_rectangle(20, 8, 4, 32)
-    ), true);
+  test_equal_i(
+      intersects(make_rectangle(16, 16, 16, 16), make_rectangle(20, 8, 4, 32)),
+      true);
 }
 
 int main(void) {
-    test_intersects();
+  test_intersects();
 
-    print_rectangle(make_rectangle(0, 0, 9, 5), make_draw_options_fill(" "));
-    puts("===");
-    print_rectangle(make_rectangle(3, 3, 10, 5), make_draw_options_fill("123"));
-    puts("===");
-    print_rectangle(make_rectangle(5, 2, 4, 6), make_draw_options_fill(":_:"));
-    puts("===");
-    print_rectangle(make_rectangle(1, 2, 4, 4), make_draw_options_flat());
+  print_rectangle(make_rectangle(0, 0, 9, 5), make_draw_options_fill(" "));
+  puts("===");
+  print_rectangle(make_rectangle(3, 3, 10, 5), make_draw_options_fill("123"));
+  puts("===");
+  print_rectangle(make_rectangle(5, 2, 4, 6), make_draw_options_fill(":_:"));
+  puts("===");
+  print_rectangle(make_rectangle(1, 2, 4, 4), make_draw_options_flat());
 
-    return 0;
+  return 0;
 }
 
 /* Beispielausgaben:
